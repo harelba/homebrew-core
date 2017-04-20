88dbcfd96ab9cb93265fe579996f0c55e1b4a498
Harel Ben-Attia <harelba@gmail.com>
2017-04-20 14:59:36 +0300

    q 1.6.3

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,8 +1,8 @@
 class Q < Formula
   desc "Treat text as a database"
   homepage "https://github.com/harelba/q"
-  url "https://github.com/harelba/q/archive/1.6.2.tar.gz"
-  sha256 "b2627b5b9b2941d7d3b206811cb6ffdaf5db73c66f541d80fbc74bfe14a0c881"
+  url "https://github.com/harelba/q/archive/1.6.3.tar.gz"
+  sha256 "56d5e15355ebd021b9e3affef919a017eac22858ba22896eccba181072811843"
   head "https://github.com/harelba/q.git"
 
   bottle :unneeded
