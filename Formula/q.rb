f772ec74d8913fc4986884f5e151d1ced847d5ae
Harel Ben-Attia <harelba@gmail.com>
2020-09-07 17:25:49 +0300

    wip

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,19 +1,20 @@
 class Q < Formula
   desc "Treat text as a database"
   homepage "https://github.com/harelba/q"
-  url "https://github.com/harelba/q/archive/1.6.3.tar.gz"
-  sha256 "56d5e15355ebd021b9e3affef919a017eac22858ba22896eccba181072811843"
+  url "https://github.com/harelba/packages-for-q/raw/master/single-binary/Darwin/2.0.9/q"
+  sha256 "88b2cd98259e64dae5ffd0efc542a8015e2ea1c13c066627847a8ff02c703b43"
+  license "GPL-3.0"
   head "https://github.com/harelba/q.git"
 
   bottle :unneeded
 
   def install
-    bin.install "bin/q"
+    bin.install "q"
   end
 
   test do
     seq = (1..100).map(&:to_s).join("\n")
-    output = pipe_output("#{bin}/q 'select sum(c1) from -'", seq)
+    output = pipe_output("#{bin}/q -c 1 'select sum(c1) from -'", seq)
     assert_equal "5050\n", output
   end
 end
